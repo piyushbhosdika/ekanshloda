91ef4a4812a0f2b927af93fc9b2bce90c0d1b21c
Piyush Bhosdika <120404024+piyushbhosdika@users.noreply.github.com>
2022-12-18 18:03:05 +0530

    Add files via upload

---
+++ 01_forkExec/1_sameProgSameCode.c
@@ -0,0 +1,23 @@
+/*
+	a) same program, same code.
+*/	
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+	int a;
+	a = fork();
+	if (a < 0)
+	{
+		printf("child process could not be created");
+		exit(-1);
+	}
+	else
+	{
+		printf("My ID is %d, My parent is %d\n", getpid(), getppid());
+	}
+	return 0;
+}
